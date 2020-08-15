9586c01c7e678ca7938dec63b4cf666111d8ec6c
sujay <sujay@pop-os.localdomain>
2020-08-15 12:26:29 +0530

    organized according to programming languages

---
+++ python/Data_Structures/Trees/basic_binary_tree.py
@@ -0,0 +1,47 @@
+# A basic binary tree can have utmost 2 children
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    # Traverse preorder(Root, Left, Right)
+    def traversePreOrder(self):
+        print(self.val, end=' ')
+        if self.left:
+            self.left.traversePreOrder()
+        if self.right:
+            self.right.traversePreOrder()
+
+    # Traverse inorder(Left, Root, Right)
+    def traverseInOrder(self):
+        if self.left:
+            self.left.traverseInOrder()
+        print(self.val, end=' ')
+        if self.right:
+            self.right.traverseInOrder()
+
+    # Traverse postorder(Left, Right, Root)
+    def traversePostOrder(self):
+        if self.left:
+            self.left.traversePostOrder()
+        if self.right:
+            self.right.traversePostOrder()
+        print(self.val, end=' ')
+
+
+root = Node(1)
+
+root.left = Node(2)
+root.right = Node(3)
+
+root.left.left = Node(4)
+
+print("Pre order Traversal: ", end="")
+root.traversePreOrder()
+print("\nIn order Traversal: ", end="")
+root.traverseInOrder()
+print("\nPost order Traversal: ", end="")
+root.traversePostOrder()
+print()
